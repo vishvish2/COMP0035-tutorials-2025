50c33679542adfc58bd82ba1d7b7a7c56d262508
vishvish2 <vishnubarath909@gmail.com>
2025-11-08 13:16:48 +0000

    5-06 adding data to db, throws InvalidRequestError

--- src/activities/db_sqlmodel/database.py
+++ src/activities/db_sqlmodel/database.py
@@ -1,8 +1,30 @@
 from __future__ import annotations
-from sqlmodel import SQLModel, create_engine
-from activities.db_sqlmodel.models import Games, Country, Disability, Team,\
+from pathlib import Path
+from sqlmodel import Session, SQLModel, create_engine
+from activities.db_sqlmodel.models import Games, Country, Disability, Team, \
     Host, GamesTeam, GamesDisability, GamesHost
+
+import pandas as pd
 
 engine = create_engine("sqlite:///paralympics_sqlmodel.db")
 
 SQLModel.metadata.create_all(engine)
+
+# 1. Read Excel file
+csv_file = Path(__file__).parent.parent.joinpath('data', 'paralympics_raw.csv')
+df = pd.read_csv(csv_file)
+
+# 2. Convert DataFrame rows of values to SQLModel instances
+records = []
+classes = [Games, Country, Disability, Team, Host, GamesTeam, GamesDisability,
+           GamesHost]
+for table in classes:
+    for _, row in df.iterrows():
+        record = table(**row.to_dict())
+        records.append(record)
+
+    # 3. Insert into database
+    engine = create_engine("sqlite:///paralympics_sqlmodel.db")
+    with Session(engine) as session:
+        session.add_all(records)
+        session.commit()
