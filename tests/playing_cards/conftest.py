2af280afa11b37d50f3917345ee142b0463c86e9
vishvish2 <vishnubarath909@gmail.com>
2025-12-28 22:06:44 +0000

    9-03 Activity: Create a fixture

--- tests/playing_cards/conftest.py
+++ tests/playing_cards/conftest.py
@@ -1,1 +1,11 @@
-# Placeholder for pytest fixtures
+import pytest
+from activities.starter.playing_cards import Suit, Rank, Deck
+
+
+@pytest.fixture
+def deck_cards():
+    suit_values = [Suit(suit=s) for s in ['Clubs', 'Diamonds', 'Hearts', 'Spades']]
+    rank_values = [Rank(rank=str(r)) for r in
+                   [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']]
+    deck_cards = Deck(suits=suit_values, ranks=rank_values)
+    yield deck_cards
